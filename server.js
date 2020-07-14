86910514ceb9648052dcd0bc6d6d439350bba18b
IsaacKlark <isaackeklarke@gmail.com>
2020-07-14 22:59:21 +0300

    created post method

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 888;
 const app = express();
-const myData = {
+let myData = {
     data1: 50,
     data2: 50
 }
 
 app.get('/api', (req, res) => {
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.json(myData);
+});
+app.post('/api/send', bodyParser.text(), (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    console.log(JSON.parse(req.body))
+    myData = JSON.parse(req.body);
 })
 app.use(express.static('build'));
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 const API_URL = process.env.REACT_APP_API_URL;
@@ -9,6 +9,8 @@ function App() {
     err => {console.log(err)})
     .then(res => {console.log(res)}, err => {console.log(err)})
   };
+  const [dataToServer1, setDataToServer1] = useState(0);
+  const [dataToServer2, setDataToServer2] = useState(0);
 
   useEffect(() => {
     getDataFromServer();
@@ -16,20 +18,41 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        type="text" 
+        placeholder="data1"
+        onBlur={e => {
+          setDataToServer1(e.target.value);
+        }}
+      />
+      <input 
+        type="text" 
+        placeholder="data2"
+        onBlur={e => {
+          setDataToServer2(e.target.value);
+        }}
+      />
+      <button 
+        type="button"
+        onClick={
+          async () => {
+            const data = {
+              data1: +dataToServer1,
+              data2: +dataToServer2
+            };
+
+            console.log(data)
+
+            await fetch(API_URL + '/send', {
+              method: 'POST',
+              body: JSON.stringify(data)
+            });
+          }
+        }
+      >
+        send
+      </button>
+        
     </div>
   );
 }
